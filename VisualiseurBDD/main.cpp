7e33e49636244a0be5a0a25462b5e49456e20b3d
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2024-02-12 17:35:39 +0100

    Fin restructuration

--- VisualiseurBDD/main.cpp
+++ VisualiseurBDD/main.cpp
@@ -1,11 +1,11 @@
-#include "Login.h"
+#include "MainWindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Login w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
